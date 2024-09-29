578113de4f58261aa4fce3b128d24fc4e1535251
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-09-30 00:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 
-const output = document.getElementById("output")
-const arr = [1,2,3,4]
+const output = document.getElementById("output");
+const arr = [1, 2, 3, 4];
 
-function delay(data,time){
-    return new Promise((resolve) => {
-     setTimeout( () => {
-           resolve (data)
-     }, time)
-    })
+function delay(data, time) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, time);
+  });
 }
 
 delay(arr, 3000)
-.then((data) => {
-     let evenArr = data.filter((ele) => ele % 2 === 0)
-     output.innerHTML = `${evenArr.join(',')}`
-	return evenArr
-}, 1000)
-.then((evenArr) => {
-     
-     let multiplies = evenArr.map((ele) => { ele * 2})
-     output.innerText = `${multiplies.join(',')}`
-}, 2000) 
+  .then((data) => {
+    const evenArr = data.filter((ele) => ele % 2 === 0);
+    output.innerHTML = `${evenArr.join(',')}`; 
+    return evenArr; 
+  })
+  .then((evenArr) => {
+    const multiplied = evenArr.map((ele) => ele * 2);
+    output.innerText = `${multiplied.join(',')}`; 
+  });
